7ea0effda95542f8659481f7b6f9779c1a6fe3fd
Sukhoon Lee <hun0975@naver.com>
2017-04-26 20:44:10 +0900

    make evaluate ONEPAIR

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -28,4 +28,31 @@ public class Evaluator {
         }
         return "NOTHING";
     }
+	
+	public String evaluate_ONEPAIR(List<Card> cardList){
+        Map<Integer, Integer> tempMap = new HashMap<Integer, Integer>();
+        int one =0;
+
+        for (Card card : cardList) {
+            if (tempMap.containsKey(card.getRank())) {
+                Integer count = tempMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                tempMap.put(card.getRank(), count);
+            } else {
+                tempMap.put(card.getRank(), new Integer(1));
+            }
+        }
+        for (Integer key : tempMap.keySet()) {
+            if (tempMap.get(key) == 2) {
+                one = one + 1;
+            }
+        }
+        if(one == 1)
+        {
+            return "ONEPAIR";
+        }
+        else{
+            return  "NOTHING";
+        }
+    }
 }

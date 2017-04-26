ba8159b85d11edcd2acf9ab1f80de02f4332a7bc
Sukhoon Lee <hun0975@naver.com>
2017-04-26 20:50:32 +0900

    make evaluate TRIPLE

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -84,4 +84,34 @@ public class Evaluator {
         }
     }
 	
+	public String evaluate_TRIPLE(List<Card> cardList){
+        Map<Integer, Integer> tempMap = new HashMap<Integer, Integer>();
+        int one =0;
+
+        for (Card card : cardList) {
+            if (tempMap.containsKey(card.getRank())) {
+                Integer count = tempMap.get(card.getRank());
+                count = new Integer(count.intValue() + 1);
+                tempMap.put(card.getRank(), count);
+            } else {
+                tempMap.put(card.getRank(), new Integer(1));
+            }
+        }
+
+        for (Integer key : tempMap.keySet()) {
+            if (tempMap.get(key) == 3) {
+                one = one + 1;
+            }
+        }
+        if(one == 1)
+        {
+            return "TRIPLE";
+        }
+        else{
+            return  "NOTHING";
+        }
+    }
+	
+	
+	
 }
